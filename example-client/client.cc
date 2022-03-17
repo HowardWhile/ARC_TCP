cbb4a0d5f364ace0f37e9e290cb140558c71548b
HowardCheng <makubex49@gmail.com>
2022-03-17 13:08:01 +0800

    make & clean 位於另一資料夾的class

--- example-client/client.cc
+++ example-client/client.cc
@@ -1,3 +1,4 @@
+#include "TCP_Client.h"
 #include <iostream>
 #include <vector>
 #include <thread>
@@ -8,10 +9,11 @@ std::mutex bufferLock;
 void bg_fun()
 {
     int i;
-    while (true)
-    {    
-        std::lock_guard<std::mutex> lock(bufferLock);    
-        {            
+    // while (true)
+    while (i < 10)
+    {
+        std::lock_guard<std::mutex> lock(bufferLock);
+        {
             buffer.push_back(i);
         }
         std::cout << "bg_fun" << i << "\r\n";
@@ -21,12 +23,14 @@ void bg_fun()
 
 int main(void)
 {
+    TCP_Client c;
+
     std::thread t1(bg_fun);
     std::thread t2(bg_fun);
     std::thread t3(bg_fun);
-    t1.join(); 
-    t2.join(); 
-    t3.join(); 
+    t1.join();
+    t2.join();
+    t3.join();
 
     std::cout << "complete\r\n";
     return 0;
---
+++ src/TCP_Client.cpp
@@ -0,0 +1,13 @@
+#include "TCP_Client.h"
+#include <iostream>
+#include <stdio.h>
+
+TCP_Client::TCP_Client()
+{
+    printf("[TCP_Client]\r\n");
+}
+
+TCP_Client::~TCP_Client()
+{
+
+}
