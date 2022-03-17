73ae525275a9ffde712eeaea10ecc9a1842163ad
HowardCheng <makubex49@gmail.com>
2022-03-17 11:43:35 +0800

    換一個makefile 測試c++11編譯 與執行序安全

--- example-client/client.cc
+++ example-client/client.cc
@@ -1,8 +1,33 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <mutex>
+
+std::vector<int> buffer;
+std::mutex bufferLock;
+void bg_fun()
+{
+    int i;
+    while (true)
+    {    
+        std::lock_guard<std::mutex> lock(bufferLock);    
+        {            
+            buffer.push_back(i);
+        }
+        std::cout << "bg_fun" << i << "\r\n";
+        i++;
+    }
+}
 
 int main(void)
 {
-    std::cout<<"HelloWorld";
+    std::thread t1(bg_fun);
+    std::thread t2(bg_fun);
+    std::thread t3(bg_fun);
+    t1.join(); 
+    t2.join(); 
+    t3.join(); 
+
+    std::cout << "complete\r\n";
     return 0;
 }
